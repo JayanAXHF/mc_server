72479dbae8799d08e4f8af46ca85b5da7d28618d
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2024-07-15 15:25:10 +0530

    The the spelling of Yajuveer Nayar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
               <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
                 Veer
               </code>
-              , Yajuveer Naiyar
+              , Yajuveer Nayar
             </li>
             <li>
               Server Admin:{" "}
